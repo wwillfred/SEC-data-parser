18274a17e6796894b0dae31f8b9f22f900273d4b
wwillfred <anicenerd+atlassian@pm.me>
2023-01-18 17:24:01 -0500

    added error checking for nonexistent keys

--- main.c
+++ main.c
@@ -2,15 +2,24 @@
 #include <json-c/json.h>
 
 int main(int argc, char **argv) {
+	
+	const char key_cik[] = "cik";
 
 	struct json_object *cik;
 
 	char relativePathName[] = "sec-data/companyfacts/CIK0000001750.json";
 	struct json_object *root = json_object_from_file(relativePathName);
-	if (!root)
+	if (!root) {
+	   printf("error, file %s not found\n", relativePathName);
 	   return 1;
+	}
 
-	json_object_object_get_ex(root, "cik", &cik);
+	json_object_object_get_ex(root, key_cik, &cik);
+	if (!cik) {
+	   printf("error, key %s not found\n", key_cik);
+	   return 1;
+	}
+
 	printf("Company CIK is: %s\n", json_object_get_string(cik));
 
 	json_object_put(root);
