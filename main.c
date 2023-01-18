6f0fe38d97276b6b9e3a399d34115338a2c9a611
wwillfred <anicenerd+atlassian@pm.me>
2023-01-18 16:21:51 -0500

    removed path construction code in favor of simpler relative pathname

--- main.c
+++ main.c
@@ -10,36 +10,11 @@ int main(int argc, char **argv) {
 
 	char buffer[4194304];
 
-	char completePathName[128];
-	char cwd[PATH_MAX];
-	char relativePathName[] = "/sec-data/companyfacts/CIK0000001750.json";
-	if (getcwd(cwd, sizeof(cwd)) != NULL) {
-	   printf("Current working dir: %s\n", cwd);
-	}
-	   else {
-	      perror("getcwd() error");
-	      return 1;
-	}
-	
-	if (sizeof(completePathName) < strlen(cwd) +1) { /* +1 is for null character */
-
-	   fprintf(stderr, "Current working directory pathname '%s' is too long\n", cwd);
-	   return 1;
-	}
-	strncpy(completePathName, cwd, sizeof(completePathName));
-
-	if (sizeof(completePathName) < (strlen(completePathName) + strlen(relativePathName) + 1) ) {
-	   fprintf(stderr, "Final size of complete pathname is too long!\n");
-	   return 1;
-	}
-	strncat(completePathName, relativePathName, (sizeof(completePathName) - strlen(completePathName) -1) ); /* -1 is for null haracter */
-	printf("Complete pathname is %s\n", completePathName);
+	char relativePathName[] = "sec-data/companyfacts/CIK0000001750.json";
 
 	struct json_object *parsed_json;
 
-	printf("Attempting to open file...\n");
-	fp = fopen(completePathName, "r");
-
+	fp = fopen(relativePathName, "r");
 
 	if (fp==NULL)
 	{
@@ -47,14 +22,11 @@ int main(int argc, char **argv) {
 	   exit(0);
 	}
 
-	printf("successfully opened file\n");
-
 	fread(buffer, 4194304, 1, fp);
 	fclose(fp);
-	printf("Just read the file\n");
 
 	parsed_json = json_tokener_parse(buffer);
-	printf("just parsed the json input:\n");
+	printf("here is the parsed json file:\n");
 	
 	puts(json_object_to_json_string_ext(parsed_json, JSON_C_TO_STRING_PRETTY));
 	
