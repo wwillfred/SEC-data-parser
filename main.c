07959dc1d743d3efd1df9b4d5b51b3e54d3ff8a5
wwillfred <anicenerd+atlassian@pm.me>
2023-01-18 17:10:35 -0500

    obtain value for cik key

--- main.c
+++ main.c
@@ -3,12 +3,18 @@
 
 int main(int argc, char **argv) {
 
+	struct json_object *cik;
+
 	char relativePathName[] = "sec-data/companyfacts/CIK0000001750.json";
 	struct json_object *root = json_object_from_file(relativePathName);
+	if (!root)
+	   return 1;
 
-	puts(json_object_to_json_string_ext(root, JSON_C_TO_STRING_PRETTY));
-	
+	json_object_object_get_ex(root, "cik", &cik);
+	printf("Company CIK is: %s\n", json_object_get_string(cik));
+
 	json_object_put(root);
+
 	return 0;
 
 }
