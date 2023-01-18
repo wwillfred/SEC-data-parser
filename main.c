ec8fe0ab938d19d7f52d303216998714e3e5ba7e
wwillfred <anicenerd+atlassian@pm.me>
2023-01-18 16:45:08 -0500

    removed unnecessary printf() call

--- main.c
+++ main.c
@@ -4,15 +4,11 @@
 int main(int argc, char **argv) {
 
 	char relativePathName[] = "sec-data/companyfacts/CIK0000001750.json";
-
 	struct json_object *root = json_object_from_file(relativePathName);
 
-	printf("here is the parsed json file:\n");
-	
 	puts(json_object_to_json_string_ext(root, JSON_C_TO_STRING_PRETTY));
 	
 	json_object_put(root);
-
 	return 0;
 
 }
