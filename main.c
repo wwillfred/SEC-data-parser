bf649fba475ca1c721c7c20a36f15812d39847e5
wwillfred <anicenerd+atlassian@pm.me>
2023-02-20 19:03:14 -0500

    Refactored iteration through quarters into its own function

--- main.c
+++ main.c
@@ -3,38 +3,17 @@
 #include <string.h>
 #include <time.h>
 
-int main(int argc, char **argv) {
 
-	char relativePathName[] = "sec-data/companyfacts/CIK0000001750.json";
-	struct json_object *root = json_object_from_file(relativePathName);
-	if (!root) {
-	   printf("error, file %s not found\n", relativePathName);
-	   return 1;
-	}
+void iterateThroughQuarters(json_object *obj)
+{
 
-	json_object *facts, *us_gaap, *netIncomeLoss, *units, *USD, *val, *fy, *fp, *it, *start, *end;
-	const char key_facts[] = "facts";
-	const char key_us_gaap[] = "us-gaap";
-	const char key_netIncomeLoss[] = "NetIncomeLoss";
-	const char key_units[] = "units";
-	const char key_USD[] = "USD";
-	const char key_val[] = "val";
-	const char key_fy[] = "fy";
-	const char key_fp[] = "fp";
+	json_object *it, *start, *end;
+
 	const char key_start[] = "start";
 	const char key_end[] = "end";
-
-	json_object_object_get_ex(root, key_facts, &facts);
-	json_object_object_get_ex(facts, key_us_gaap, &us_gaap);
-	json_object_object_get_ex(us_gaap, key_netIncomeLoss, &netIncomeLoss);
-	json_object_object_get_ex(netIncomeLoss, key_units, &units);
-	json_object_object_get_ex(units, key_USD, &USD);
-
-	const char *str;
-	int n = json_object_array_length(USD);
-
-	int quarterValues[4];
 	
+	int n = json_object_array_length(obj);
+
 	struct tm tm_start;
 	struct tm tm_end;
 	char *buf_start;
@@ -42,7 +21,7 @@ int main(int argc, char **argv) {
 	
 	for (int i=0; i<n; i++)
 	{
-	  it = json_object_array_get_idx(USD, i);
+	  it = json_object_array_get_idx(obj, i);
 	  json_object_object_get_ex(it, key_start, &start);
 	  buf_start = strdup(json_object_get_string(start));
           strptime(buf_start, "%Y-%m-%d", &tm_start);
@@ -53,6 +32,34 @@ int main(int argc, char **argv) {
 
 	  printf("The period starts on %s and ends on %s\n", buf_start, buf_end);
 	}
+}
+
+int main(int argc, char **argv) {
+
+	char relativePathName[] = "sec-data/companyfacts/CIK0000001750.json";
+	struct json_object *root = json_object_from_file(relativePathName);
+	if (!root) {
+	   printf("error, file %s not found\n", relativePathName);
+	   return 1;
+	}
+
+	json_object *facts, *us_gaap, *netIncomeLoss, *units, *USD, *val, *fy, *fp;
+	const char key_facts[] = "facts";
+	const char key_us_gaap[] = "us-gaap";
+	const char key_netIncomeLoss[] = "NetIncomeLoss";
+	const char key_units[] = "units";
+	const char key_USD[] = "USD";
+	const char key_val[] = "val";
+	const char key_fy[] = "fy";
+	const char key_fp[] = "fp";
+
+	json_object_object_get_ex(root, key_facts, &facts);
+	json_object_object_get_ex(facts, key_us_gaap, &us_gaap);
+	json_object_object_get_ex(us_gaap, key_netIncomeLoss, &netIncomeLoss);
+	json_object_object_get_ex(netIncomeLoss, key_units, &units);
+	json_object_object_get_ex(units, key_USD, &USD);
+
+	iterateThroughQuarters(USD);
 
 	//printf("the object from key %s is: %s\n", key_USD, json_object_get_string(USD));	
 
@@ -61,3 +68,4 @@ int main(int argc, char **argv) {
 	return 0;
 
 }
+
